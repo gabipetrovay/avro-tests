59c50c5b019813ccb42b01cc76421320cbf9d32c
Gabriel Petrovay <gabipetrovay@gmail.com>
2020-03-26 08:58:22 +0100

    Initial commit

---
+++ src/test/java/com/example/PropertyReader.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.junit.BeforeClass;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyReader {
+
+    private static Properties properties;
+
+    @BeforeClass
+    public static void initializeProperties() throws FileNotFoundException, IOException {
+
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        String appConfigPath = rootPath + "application.properties";
+
+        properties = new Properties();
+        properties.load(new FileInputStream(appConfigPath));
+
+    }
+
+    protected static String getProperty(String key) {
+
+        return properties.getProperty(key);
+
+    }
+}
