b6af79df85300ecbbdb48676f087c44fa40d139c
Gabriel Petrovay <gabipetrovay@gmail.com>
2020-04-03 13:55:41 +0200

    Added a test for a real Kafka with Schema Registry and SSL

--- src/test/java/com/example/PropertyReader.java
+++ src/test/java/com/example/PropertyReader.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeAll;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 public class PropertyReader {
@@ -22,9 +24,26 @@ public class PropertyReader {
 
     }
 
+    public static String resolveRelativeResourcesPath(String relativeresourcePath) {
+
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        Path resolvedPath = Paths.get(rootPath, relativeresourcePath);
+        return resolvedPath.toAbsolutePath().toString();
+    }
+
     protected static String getProperty(String key) {
 
         return properties.getProperty(key);
 
+    }
+
+    protected static String getProperty(String key, String defaultValue) {
+
+        String value = getProperty(key);
+        if (value == null) {
+            return defaultValue;
+        }
+
+        return value;
     }
 }
--- src/test/java/com/example/avro/TestKafkaAvroSslConsumer.java
+++ src/test/java/com/example/avro/TestKafkaAvroSslConsumer.java
@@ -4,59 +4,32 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.example.PropertyReader;
 
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClientConfig;
 import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
-import io.confluent.kafka.serializers.KafkaAvroSerializer;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.config.SslConfigs;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.time.Duration;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
-@TestMethodOrder(OrderAnnotation.class)
-public class TestKafkaAvroValues extends PropertyReader {
+public class TestKafkaAvroSslConsumer extends PropertyReader {
 
-    private static final String STATIC_KEY = "static-key";
-
-    private static Producer<String, String> producer;
     private static Consumer<String, String> consumer;
-
-    @BeforeAll
-    public static void initializeProducer() throws UnknownHostException {
-
-        Properties config = new Properties();
-
-        config.put(ProducerConfig.CLIENT_ID_CONFIG, InetAddress.getLocalHost().getHostName());
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getCanonicalName());
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getCanonicalName());
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, getProperty("kakfa.bootstrap.servers"));
-        config.put(ProducerConfig.ACKS_CONFIG, "all");
-        config.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, getProperty("kafka.schema-registry.url"));
-
-        producer = new KafkaProducer<String, String>(config);
-    }
 
     @BeforeAll
     public static void initializeConsumer() throws UnknownHostException {
@@ -73,6 +46,28 @@ public class TestKafkaAvroValues extends PropertyReader {
 
         config.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, getProperty("kafka.schema-registry.url"));
 
+        if (getProperty("kafka.ssl.truststore.location") != null) {
+            // Kakfa consumer SSL configuration
+            config.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
+            config.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG,
+                    resolveRelativeResourcesPath(getProperty("kafka.ssl.truststore.location")));
+            config.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, getProperty("kafka.ssl.truststore.password"));
+            config.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG,
+                    resolveRelativeResourcesPath(getProperty("kafka.ssl.keystore.location")));
+            config.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, getProperty("kafka.ssl.keystore.password"));
+            config.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, getProperty("kafka.ssl.key.password"));
+
+            // Kakfa Registry client SSL configuration
+            config.put(SchemaRegistryClientConfig.CLIENT_NAMESPACE + SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG,
+                    config.getProperty(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG));
+            config.put(SchemaRegistryClientConfig.CLIENT_NAMESPACE + SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG,
+                    config.getProperty(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG));
+            config.put(SchemaRegistryClientConfig.CLIENT_NAMESPACE + SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG,
+                    config.getProperty(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG));
+            config.put(SchemaRegistryClientConfig.CLIENT_NAMESPACE + SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG,
+                    config.getProperty(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG));
+        }
+
         consumer = new KafkaConsumer<String, String>(config);
 
         String topic = getProperty("kafka.topics.avro");
@@ -85,34 +80,10 @@ public class TestKafkaAvroValues extends PropertyReader {
         consumer.close();
     }
 
-    @AfterAll
-    public static void closeProducer() throws UnknownHostException {
-
-        producer.close();
-    }
-
     @Test
-    @Order(1)
-    public void testProduceMessageSync() throws InterruptedException, ExecutionException {
-
-        String topic = getProperty("kafka.topics.avro");
-
-        final ProducerRecord<String, String> record = new ProducerRecord<>(topic, STATIC_KEY,
-                "this is the string message at " + new Date());
-        Future<RecordMetadata> future = producer.send(record);
-        producer.flush();
-
-        RecordMetadata metadata = future.get();
-        System.out.println("Timestamp: " + new Date(metadata.timestamp()));
-        System.out.println("Offset: " + metadata.offset());
-
-    }
-
-    @Test
-    @Order(2)
     public void testConsumeMessageSync() throws InterruptedException, ExecutionException {
 
-        ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(500));
+        ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(5000));
 
         System.out.println("Record count: " + records.count());
 
